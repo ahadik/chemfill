4c9229e18da923118737792d23a1e66ad1bffce1
Alexander Hadik <alex@transcriptic.com>
2019-03-05 13:31:21 -0800

    Add hidden files

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "extends": "airbnb",
+    "globals": {
+        "context": false,
+        "nil": false,
+        "NSURL": false,
+        "NSURLRequest": false,
+        "fetch": false,
+        "NSProcessInfo": false,
+        "NSFileManager": false,
+        "NSURLConnection": false,
+        "NSTemporaryDirectory": false,
+        "MSImageData": false,
+        "NSImage": false,
+        "error": true,
+        "NSWorkspace": false
+    }
+};
